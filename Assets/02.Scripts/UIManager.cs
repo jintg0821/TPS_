5b5d89d427439ba9f729d905c248a135a7011b13
jintg0821 <jintg0825@gmail.com>
2023-07-13 19:28:07 +0900

    끝^^_

--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -8,14 +8,17 @@ public class UIManager : MonoBehaviour
     public Button startButton;
     public Button optionButton;
     public Button shopButton;
+
     private UnityAction action;
     void Start()
     {
         // UnityAction을 사용한 이벤트 연결 방식
         action = () => OnButtonClick(startButton.name);
         startButton.onClick.AddListener(action);
+
         // 무명 메서드를 활용한 이벤트 연결 방식
         optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
+
         // 람다식을 활용한 이벤트 연결 방식
         shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
     }
